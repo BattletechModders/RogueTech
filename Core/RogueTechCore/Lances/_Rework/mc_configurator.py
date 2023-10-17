da8b61104df3f278f26b5e7b888262132c1fa0a5
h4ilst0rm <1belownull@gmail.com>
2023-10-17 23:30:55 +0300

    new support lance numbers revision 1

--- Core/RogueTechCore/Lances/_Rework/mc_configurator.py
+++ Core/RogueTechCore/Lances/_Rework/mc_configurator.py
@@ -13,13 +13,13 @@ for diff in range(1, 50+1):
     ally_lances = []
     opfor_lances = []
 
-    if diff < 5:
-        ally_lances = ["Damaged_Lightly_MCSupport", "Damaged_Heavily_MCSupport"]
-        opfor_lances = ["Damaged_Lightly_MCSupport", "Damaged_Heavily_MCSupport"]
-    elif diff < 10:
+    if diff < 4:
+        ally_lances = ["Standard_MCSupport", "Damaged_Lightly_MCSupport", "Damaged_Heavily_MCSupport"]
+        opfor_lances = ["Standard_MCSupport", "Damaged_Lightly_MCSupport", "Damaged_Heavily_MCSupport"]
+    elif diff < 8:
         ally_lances = ["Standard_MCSupport", "Damaged_Lightly_MCSupport"]
         opfor_lances = ["Standard_MCSupport", "Damaged_Lightly_MCSupport"]
-    elif diff < 15:
+    elif diff < 12:
         ally_lances = ["Standard_MCSupport"]
         opfor_lances = ["Standard_MCSupport", "Standard_Support"]
     else:
@@ -32,6 +32,19 @@ for diff in range(1, 50+1):
     diff_config["Enemy"]["LancePool"]["ALL"] = opfor_lances
     diff_config["Allies"]["LancePool"] = {}
     diff_config["Allies"]["LancePool"]["ALL"] = ally_lances
+
+    diff_config["Enemy"]["EliteLances"] = {}
+
+    diff_config["Allies"]["Max"] = 1
+    diff_config["Enemy"]["Max"] = 1 if diff < 10 else 2 
+
+    # .5 .45 .4 .3 ... .05 .01 .01 .01
+    diff_config["Allies"]["ChanceToSpawn"] = round(max(0.01, 0.5 - 0.05*(diff-1)), ndigits=3)
+
+    if diff < 10:
+        diff_config["Enemy"]["ChanceToSpawn"] = round(0.1 + 0.05*(diff-1), ndigits=3)
+    else:
+        diff_config["Enemy"]["ChanceToSpawn"] = round(0.25 + 0.05*(diff-10), ndigits=3)
 
     with open(file_path, 'w', newline='\r\n') as new_file:
         json.dump(diff_config, new_file, indent=2)
